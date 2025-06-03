7d7db2298b57fb599e2b55536bb720119845c21c
rushichavan21 <rushichavan2327@gmail.com>
2025-06-03 14:53:46 +0530

    tailwind fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <div className="bg-red-500 text-white p-4">Tailwind Test</div> */}
     <App />
   </React.StrictMode>
 );
